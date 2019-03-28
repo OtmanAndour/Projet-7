2a131d308377251db40fc433b0b88bcea7f84763
Othman Andour <andour.otman@gmail.com>
2019-03-28 10:43:40 +0100

    Added style and js script in static folder

---
+++ static/app.js
@@ -0,0 +1,22 @@
+$('#form').submit(function (e) {
+    e.preventDefault()
+    var mavariable = $('#input').val()
+    $.get('test?name=' + mavariable, function (result) {
+        result = JSON.parse(result)
+        console.log(result)
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: { lat: result.latitude, lng: result.longitude },
+            zoom: 15
+        });
+        marker = new google.maps.Marker({
+            position: { lat: result.latitude, lng: result.longitude },
+            map: map
+        });
+        for (let page in result.page.query.pages) {
+            $("#page").html(result.page.query.pages[page].extract);
+            break
+        }
+
+    })
+
+}) 
