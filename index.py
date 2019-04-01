9464ac22b099a778a53821122b6db6bcde80f170
Othman Andour <andour.otman@gmail.com>
2019-04-01 15:01:22 +0200

    Added bootstrap styling

--- index.py
+++ index.py
@@ -1,18 +1,47 @@
 from flask import Flask,request
-from key import *
+from constants import *
 import requests
 import json
 
 
+
 app = Flask(__name__)
 
-search_url = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json"
-wiki_url = "https://fr.wikipedia.org/w/api.php"
+
+
+#def parse(research):
+#        """Parse the research"""
+#        research_parsed = ""
+#        for word in research.split():            #Parsing the research
+#                if word not in base_word:
+#                        research_parsed = research_parsed + word + " "             
+#        research_parsed = research_parsed[:-1]          #Parsed researched
+#        return research_parsed
+#
+#def google_maps_api_search(search):
+#    search_params = {'key' : api_key, 'input' : search, 'inputtype' : 'textquery', 'fields' : 'geometry'}
+#    search_req = requests.get(search_url, params=search_params)
+#    search_json = search_req.json()
+#    return search_json
+#
+#def get_latitude(result):
+#        return result["candidates"][0]["geometry"]["location"]["lat"]
+#
+#def get_longitutde(result):
+#        return result["candidates"][0]["geometry"]["location"]["lng"]
+#
+#
+#def wikipedia_api_search(search,params):
+#    wiki_req = requests.Session().get(wiki_url, params=params)
+#    wiki_json = wiki_req.json()
+#    return wiki_json
+#
 
 @app.route('/')  #Main route for the html webpage
 def helloIndex():
     with open('templates/index.html', 'r') as f:
         return f.read()
+        
 
 @app.route('/test')  #Route for another webpage
 def testIndex():
@@ -25,33 +54,36 @@ def testIndex():
     search_params = {'key' : api_key, 'input' : var_parsed, 'inputtype' : 'textquery', 'fields' : 'geometry'}
     search_req = requests.get(search_url, params=search_params)
     search_json = search_req.json()
-    print(var_parsed)
-    #print (search_json)
-    latitude = search_json["candidates"][0]["geometry"]["location"]["lat"]
-    longitude = search_json["candidates"][0]["geometry"]["location"]["lng"]
+    if search_json["status"] == 'ZERO_RESULTS':
+            print("Je ne vois pas de quel endroit tu parles, mon poussin")
+            return json.dumps("No results")
+    else:
+        latitude = search_json["candidates"][0]["geometry"]["location"]["lat"]
+        longitude = search_json["candidates"][0]["geometry"]["location"]["lng"]
 
-    wiki_params = {'action': "query",'list':"search",'srsearch': var_parsed, 'format': "json" }    
-    wiki_req = requests.Session().get(wiki_url, params=wiki_params)
-    wiki_json = wiki_req.json()
-    pageid = wiki_json['query']['search'][0]['pageid']
-    
-    wiki_params =  {
-        'pageids': pageid,
-        'action':'query',
-        'utf8':True,
-        'format':'json',
-        'prop':'extracts',
-        'exlimit':1,
-        'explaintext':True,
-        'exsentences':3,
-        'exsectionformat':'plain',
-        'exintro':True,}
-    wiki_req = requests.Session().get(wiki_url, params=wiki_params)
-    wiki_json = wiki_req.json()
-    print(wiki_json)
+        wiki_params = {'action': "query",'list':"search",'srsearch': var_parsed, 'format': "json" }    
+        wiki_req = requests.Session().get(wiki_url, params=wiki_params)
+        wiki_json = wiki_req.json()
+        pageid = wiki_json['query']['search'][0]['pageid']
+        
+        wiki_params =  {
+                'pageids': pageid,
+                'action':'query',
+                'utf8':True,
+                'format':'json',
+                'prop':'extracts|info',
+                'inprop':'url',
+                'exlimit':1,
+                'explaintext':True,
+                'exsentences':3,
+                'exsectionformat':'plain',
+                'exintro':True,}
+        wiki_req = requests.Session().get(wiki_url, params=wiki_params)
+        wiki_json = wiki_req.json()
+        print(wiki_json)
 
-    dico = {'latitude' : latitude, 'longitude' : longitude, 'page' : wiki_json}
-    
-    return json.dumps(dico)
+        dico = {'latitude' : latitude, 'longitude' : longitude, 'page' : wiki_json}
+        
+        return json.dumps(dico)
 
 app.run(port= 80)
--- static/app.js
+++ static/app.js
@@ -4,20 +4,30 @@ $('#form').submit(function (e) {
     console.log(mavariable)
     $.get('test?name=' + mavariable, function (result) {
         result = JSON.parse(result)
-        console.log(result)
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: { lat: result.latitude, lng: result.longitude },
-            zoom: 15
-        });
-        marker = new google.maps.Marker({
-            position: { lat: result.latitude, lng: result.longitude },
-            map: map
-        });
-        for (let page in result.page.query.pages) {
-            $("#page").html(result.page.query.pages[page].extract);
-            break
-        }
+        if (result === "No results"){
+            $("#text").html("Est-ce que tu peux répéter, mon poussin? Je n'ai pas bien compris. Tu sais, je me fait vieux, je n'entend plus très bien. D'ailleurs, ça me rapelle une histoire que j'ai vécu en 1935. A cet époq- Zzzz...")
+        } else{
+            console.log(result)
+            $("#text1").html("Bien sur mon poussin, là voici! Mais laisse-moi d'abord te raconter le lieu de cette histoire!");
+            $("#text2").html("C'était en 1957, ...");
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: { lat: result.latitude, lng: result.longitude },
+                zoom: 15
+            });
+            marker = new google.maps.Marker({
+                position: { lat: result.latitude, lng: result.longitude },
+                map: map
+            });
+            for (let page in result.page.query.pages) {
+                let extract = result.page.query.pages[page].extract;
+                let url = result.page.query.pages[page].fullurl;
+                console.log(url);
+                $("#blockquote").html(extract);
+                $("#blockquote").append('<footer class="blockquote-footer">Wikipedia. Pour en savoir plus, cliquer sur ce <a href="' + url + '">lien</a></footer>');
+                break
+            }
 
+        }
     })
 
 }) 
