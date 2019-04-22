66ad6e0d619d1777b4e35d750bb417333de192cf
Othman Andour <andour.otman@gmail.com>
2019-04-22 07:23:43 +0200

    Changed site a little

--- static/app.js
+++ static/app.js
@@ -1,16 +1,21 @@
 $('#form').submit(function (e) {
     e.preventDefault()
-    var mavariable = $('#input').val()
-    console.log(mavariable)
-    $.get('result?name=' + mavariable, function (result) {
+    var input = $('#input').val()
+    $("#text").html("Vous : " + input + "<br><br>")
+    $("#text").append('<img id="loader" src="http://loadinggif.com/generated-image?imageId=31&bgColor=%23ccc&fgColor=%23000000&transparentBg=0&download=0&random=0.00030365096131301783" />')
+    $('#loader').show()
+    console.log(input)
+    $.get('result?name=' + input, function (result) {
         result = JSON.parse(result)
         if (result === "No results"){
-            $("#text").html("Est-ce que tu peux répéter, mon poussin? Je n'ai pas bien compris. Tu sais, je me fait vieux, je n'entend plus très bien. D'ailleurs, ça me rapelle une histoire que j'ai vécu en 1935. A cet époq- Zzzz...")
+            $('#loader').hide()
+            $("#text").append("GrandPy : Est-ce que tu peux répéter, mon poussin? Je n'ai pas bien compris. Tu sais, je me fait vieux, je n'entend plus très bien. D'ailleurs, ça me rapelle une histoire que j'ai vécu en 1935. A cet époq- Zzzz...")
             $("#map").empty();
             $("#blockquote").empty();
         } else{
             console.log(result)
-            $("#text").html("Bien sur mon poussin, là voici! Mais laisse-moi d'abord te raconter le lieu de cette histoire!<br><br>");
+            $('#loader').hide()
+            $("#text").append("GrandPy : Bien sur mon poussin, là voici! Mais laisse-moi d'abord te raconter le lieu de cette histoire!<br><br>");
             $("#text").append(result.quote);
             map = new google.maps.Map(document.getElementById('map'), {
                 center: { lat: result.latitude, lng: result.longitude },
